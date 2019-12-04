4ab422e329fd2b7146746004775160453025c356
gswilson6 <gswilson6@gmail.com>
2019-12-04 11:57:40 -0500

    this is actually where I added the flask app

---
+++ flask_recommender/brewery_recommender/routes.py
@@ -0,0 +1,202 @@
+import os
+import secrets
+import pandas as pd
+import numpy as np
+import pickle
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request, abort
+from brewery_recommender import app, db, bcrypt
+from brewery_recommender.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from brewery_recommender.models import User, Post
+from brewery_recommender.recommender import data_manipulation
+
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+@app.route("/about/")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        username = str(user.username)
+        with open("username.txt", "wb") as fp:
+            pickle.dump(username, fp)
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    df = pd.read_csv('../data/beer_reviews_col_drop.csv')
+    beers = list(df['beer_name'].unique())
+    return render_template('create_post.html', beers=beers, title='New Beer', legend='New Post')
+
+
+@app.route('/beer_adder')
+def test():
+    beer_ratings = request.args
+    beers = str(beer_ratings['beer'])
+    ratings = float(beer_ratings['rating'])
+    post = Post(title=beers, content=ratings, author=current_user)
+    db.session.add(post)
+    db.session.commit()
+    flash('Your have successfully added your beer!')
+    return redirect(url_for('home'))
+
+
+@app.route('/recommendations')
+def recommendations():
+    with open("username.txt", "rb") as fp:
+        username = pickle.load(fp)
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    beers = []
+    ratings = []
+    for post in user.posts:
+        beers.append((post.__dict__['title']))
+        ratings.append((post.__dict__['content']))
+    beer_ratings = {}
+    for i in range(len(beers)):
+        beer_ratings[beers[i]] = ratings[i]
+    recommendations = data_manipulation(beer_ratings)
+    beers = recommendations[1]
+    return render_template('recommendations.html', recommendations=beers)
+    # return render_template('sql_test.html', beers=beer_ratings, ratings=ratings)
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+
+@app.route("/your_beers")
+def your_beers():
+    with open("username.txt", "rb") as fp:
+        username = pickle.load(fp)
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=50)
+    return render_template('your_beers.html', posts=posts, user=user)
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=50)
+    return render_template('user_posts.html', posts=posts, user=user)
